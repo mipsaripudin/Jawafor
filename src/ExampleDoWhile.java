31671c7edf84b3d0c5abf3fafe2577f548826dda
mipsaripudin <mip.saripudin@unida.ac.id>
2021-12-18 11:16:54 +0700

    Javafor

---
+++ src/ExampleDoWhile.java
@@ -0,0 +1,11 @@
+
+public class ExampleDoWhile {
+public static void main (String[] args) {
+	int number = 0;
+	do {
+		System.out.println("Angka Lebih Besar dari 10");
+	}while (number > 10);
+	System.out.println("Pernyataan ini Berasal dari luar do while loop");
+	}
+}
+
---
+++ src/Excamplewhile.java
@@ -0,0 +1,10 @@
+
+public class Excamplewhile {
+	public static void main (String[] args) {
+		int number = 0;
+		while (number > 10) {
+			System.out.println("Angka Lebih Besar dari 10");
+		}
+		System.out.println("Pernyataan ini berasal dari Luar while loop");
+	}
+}
